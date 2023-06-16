1a5b02f48ebf1c4972676487828c539af8d63fd6
Chris Miller <chrmiller@microsoft.com>
2023-06-16 12:19:39 -0400

    Bug terminology

--- src/systems/game/ui.rs
+++ src/systems/game/ui.rs
@@ -6,9 +6,9 @@ use super::{ClearProducts, GameState};
 fn ui_system(mut contexts: EguiContexts, mut state: ResMut<GameState>, mut clear_products: EventWriter<ClearProducts>) {
   egui::Window::new("Operations Menu").show(contexts.ctx_mut(), |ui| {
     ui.vertical_centered_justified(|ui| {
-      ui.label(format!("Currency: ${}", state.currency));
+      ui.label(format!("Bug Guts: {}", state.currency));
     });
-    let run_button_text = if state.run { "Stop Spawners" } else { "Start Spawners" };
+    let run_button_text = if state.run { "Stop Hives" } else { "Start Hives" };
     ui.vertical_centered_justified(|ui| {
       if ui.button(run_button_text).clicked() {
         clear_products.send_default();
