577c5807ec6440d32fb37558d6bb309307858340
Chris Miller <chrmiller@microsoft.com>
2023-06-16 11:42:30 -0400

    Cant right click on web

--- src/systems/game/camera.rs
+++ src/systems/game/camera.rs
@@ -1,9 +1,9 @@
 use bevy::prelude::*;
 use smooth_bevy_cameras::controllers::fps::FpsCameraController;
 
-fn handle_camera_pan_right_mouse_btn(buttons: Res<Input<MouseButton>>, mut camera: Query<&mut FpsCameraController>) {
+fn handle_camera_pan_right_mouse_btn(keyboard_input: Res<Input<KeyCode>>, mut camera: Query<&mut FpsCameraController>) {
   if let Ok(mut camera) = camera.get_single_mut() {
-    let sentitivity = if buttons.pressed(MouseButton::Right) {
+    let sentitivity = if keyboard_input.pressed(KeyCode::Space) {
       Vec2::splat(0.3)
     } else {
       Vec2::ZERO
