6594286f9961d738fc2ec8a3da3a3fdf9e038fc9
Chris Miller <chrmiller@microsoft.com>
2023-06-16 11:45:28 -0400

    On Q

--- src/systems/game/camera.rs
+++ src/systems/game/camera.rs
@@ -3,7 +3,7 @@ use smooth_bevy_cameras::controllers::fps::FpsCameraController;
 
 fn handle_camera_pan_right_mouse_btn(keyboard_input: Res<Input<KeyCode>>, mut camera: Query<&mut FpsCameraController>) {
   if let Ok(mut camera) = camera.get_single_mut() {
-    let sentitivity = if keyboard_input.pressed(KeyCode::Space) {
+    let sentitivity = if keyboard_input.pressed(KeyCode::Q) {
       Vec2::splat(0.3)
     } else {
       Vec2::ZERO
